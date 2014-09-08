1756bb17ee91360d59f18483d82519a90e70ae6e
Gurjot Manak <gurjotsinghmanak@gmail.com>
2014-09-08 17:47:14 +0530

    changes in volunteer flow

---
+++ volunteer-map.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("inc/header.inc.php");
+
+page_render('volunteer-map.html', array(
+	'page_title' => 'School Map',
+	'header_full_width' => true,
+	'header_fixed' => true,
+	'hide_footer' => true
+));
+
+
