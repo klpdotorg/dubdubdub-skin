b2154c0420327b7151ff924e5e8b3c48f16141d5
Gautam Chaudhary <gkcgautam@gmail.com>
2014-10-21 10:47:33 +0530

    Donation page - Login state changes

--- donate-required-for-library.php
+++ donate-required-for-library.php
@@ -7,7 +7,18 @@ $page = array(
 	'logged_in' => false
 );
 
+$page['user_details'] = array(
+	'name' => 'Gautam Chaudhary',
+	'email' => 'gkcgautam@gmail.com',
+	'phone' => '9876543210',
+	'address' => '#123, Random Street, Chandigarh',
+);
+
 if(isset($_POST['submit'])){
+	$page['logged_in'] = true;
+}
+
+if(isset($_GET['logged_in'])){
 	$page['logged_in'] = true;
 }
 
